c6435c17a547d2cfd8601e24297fda23d6185e53
kaifergerstrom <kaifergerstrom@gmail.com>
2019-06-05 16:09:50 -0400

    Bare bones tankdrive code

--- src/main/java/frc/robot/commands/TankDrive.java
+++ src/main/java/frc/robot/commands/TankDrive.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,16 +7,15 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.OI;
 import frc.robot.Robot;
 
-/**
- * An example command.  You can replace me with your own command.
- */
-public class ExampleCommand extends Command {
-  public ExampleCommand() {
+public class TankDrive extends Command {
+  public TankDrive() {
     // Use requires() here to declare subsystem dependencies
-    requires(Robot.m_subsystem);
+    // eg. requires(chassis);
   }
 
   // Called just before this Command runs the first time
@@ -27,6 +26,25 @@ public class ExampleCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+
+    // Left joystick controller side
+    if (Math.abs(OI.driver.getY(Hand.kLeft)) < .1) {
+      Robot.drivetrain.talon_BL.set(0);
+      Robot.drivetrain.talon_FL.set(0);
+    } else {
+      Robot.drivetrain.talon_FL.set(OI.driver.getY(Hand.kLeft));
+      Robot.drivetrain.talon_BL.set(OI.driver.getY(Hand.kLeft));
+    }
+    
+    // Right joystick controller side
+    if (Math.abs(OI.driver.getY(Hand.kRight)) < .1) { 
+      Robot.drivetrain.talon_FR.set(0);
+      Robot.drivetrain.talon_BR.set(0);
+    } else {
+      Robot.drivetrain.talon_FR.set(OI.driver.getY(Hand.kRight));
+      Robot.drivetrain.talon_BR.set(OI.driver.getY(Hand.kRight));
+    }
+
   }
 
   // Make this return true when this Command no longer needs to run execute()
